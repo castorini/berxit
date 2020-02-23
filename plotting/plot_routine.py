546bff98de1a52607abfb91acddfa1202db52648
Ji Xin <xinji1996@gmail.com>
2020-02-23 15:21:47 -0500

    Update routine plot color scheme

--- plotting/plot_routine.py
+++ plotting/plot_routine.py
@@ -7,12 +7,26 @@ import seaborn as sns
 
 model = sys.argv[1]
 
+formal_name = {
+    "shrink-1": "weight-tok",
+    "all": "joint"
+}
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+color = {
+    "two_stage": 0,
+    "joint": 1,
+    "weight-tok": 2,
+    "all_alternate": 3
+}
+
 def np_load(fname):
     return np.load(fname, allow_pickle=True)
 
 datasets = ["RTE", "MRPC", "SST-2", "QNLI", "QQP", "MNLI"]
 sizes = ["2.5k", "3.5k", "67k", "108k", "363k", "392k"]
-routines = ["two_stage", "all", "shrink-1", "self_distil"]
+routines = ["two_stage", "all", "shrink-1", "all_alternate"]
 M, N = 3, len(datasets)//3
 fig, axes = plt.subplots(M, N, figsize=[N*4, M*4])
 axes.reshape([-1])
@@ -21,9 +35,9 @@ def plot_acc_by_layer(axis, datafile, title):
     # M = [max(d[0]) for d in data if d[1]=='two_stage'][0]
     for d in data:
         # d[0] = [x/M for x in d[0]]
-        if d[1]=='shrink-1':
-            d[1] = 'weight-tok'
-        axis.plot(d[0], 'o-', label=d[1],
+        if d[1] in formal_name:
+            d[1] = formal_name[d[1]]
+        axis.plot(d[0], 'o-', color=colors[color[d[1]]], label=d[1],
                   linewidth=1, markersize=1)
     axis.legend(loc='lower right', fontsize=15)
     axis.set_title(title, fontsize=15)
