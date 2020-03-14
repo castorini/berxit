ed84604c512b2668a2108304f78fbb259ba1637c
Ji Xin <xinji1996@gmail.com>
2020-03-14 11:19:46 -0400

    Update plot

--- plotting/entropy_analysis.py
+++ plotting/entropy_analysis.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 model = sys.argv[1]
+num_layers = 12 if model.endswith("base") else 24
 
 analyze_entropy_per_layer = False
 if analyze_entropy_per_layer:
@@ -25,48 +26,52 @@ if analyze_entropy_per_layer:
             axes[i].plot(data[sel])
 
     plt.show()
+    exit(0)
     # plt.tight_layout()
     # plt.savefig("entropy_violin_{}.pdf".format(model))
 
 dataset, routine = sys.argv[2:]
 filepath = f"saved_models/{model}/{dataset}/{routine}-42/"
 entropy_col = np.load(filepath+"entropy_distri.npy")
+maxlogit_col = np.load(filepath+"maxlogit_distri.npy")
 correct_col = []
-for i in range((12 if model.endswith("base") else 24) - 1):
+for i in range(num_layers - 1):
     correct_col.append(np.load(filepath+"correctness_layer{}.npy".format(i)))
 correct_col.append(np.load(filepath+"correctness_layer-1.npy"))
 correct_col = np.transpose(correct_col)
 n = len(entropy_col)
 
-analyze_entropy_and_correctness = False
+analyze_entropy_and_correctness = True
 if analyze_entropy_and_correctness:
 
     fig, axes = plt.subplots(12, 12, figsize=[20, 20])
     axes = axes.reshape(-1)
-    def plot_unit(axis, ent, cor):
+    def plot_unit(axis, ent, cor, mlog):
         axis.plot(ent)
-        axis.plot(cor/2)
-        axis.set_ylim(0, 0.7)
+        axis.plot(cor*1.1)
+        axis.plot(mlog)
+        # axis.set_ylim(0, 0.7)
+        axis.set_yticks([])
 
     i = 0
     easy = 0
-    exception = 0
+    wrong = 0
     for x in range(n):
-        if entropy_col[x][3]<entropy_col[x][2] and entropy_col[x][2]<entropy_col[x][1] and entropy_col[x][1]<entropy_col[x][0]:
-            if correct_col[x][3]==0:
-                print("!", x)
-                exception += 1
+        if entropy_col[x][0]<0.05:
             easy += 1
-        else:
-            if i<144:
-                plot_unit(axes[i], entropy_col[x], correct_col[x])
+            if correct_col[x][0]==0:
+                wrong += 1
+        if i<144:
+            plot_unit(axes[i], entropy_col[x], correct_col[x], maxlogit_col[x])
             i += 1
 
-    print(easy, exception)
-    plt.show()
+    print(n, easy, wrong)
+    # plt.show()
+    plt.tight_layout()
+    plt.savefig(f"entropy_trend-{model}-{dataset}-{routine}")
 
 
-analyze_correlation = True
+analyze_correlation = False
 if analyze_correlation:
     fig, axes = plt.subplots(1, 1)
     points = [[], []]
