283742a71db855459e40363be75b48d847825a8e
Ji Xin <xinji1996@gmail.com>
2020-05-04 13:45:41 -0400

    Minor fix

--- plotting/collect_limit.py
+++ plotting/collect_limit.py
@@ -8,13 +8,14 @@ path, model_size = sys.argv[1:]
 layers = 12 if model_size == 'base' else 24
 
 col = []
-for i in range(layers):
+for i in range(layers-1):
     try:
         data = np.load(path+f'/layer-{i}.npy')
     except FileNotFoundError:
         print('File Not Found at Layer ' + str(i))
         exit(1)
     col.append(float(data))
+col.append(float(np.load(path+'/../two_stage-42/each_layer.npy')[-1]))
 
 try:
     shutil.copyfile(path+'/each_layer.npy', path+'/each_layer.npy.backup')
